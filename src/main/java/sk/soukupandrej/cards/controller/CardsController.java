8e1624d2ba5ecf841a15b5f33d68cdd0fa340913
soukupa <soukup.andrej@gmail.com>
2023-03-06 21:44:17 +0100

    initial commit 2

--- src/main/java/sk/soukupandrej/cards/controller/CardsController.java
+++ src/main/java/sk/soukupandrej/cards/controller/CardsController.java
@@ -1,11 +1,17 @@
 package sk.soukupandrej.cards.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sk.soukupandrej.cards.config.CardServiceConfig;
 import sk.soukupandrej.cards.model.Cards;
 import sk.soukupandrej.cards.model.Customer;
+import sk.soukupandrej.cards.model.Properties;
 import sk.soukupandrej.cards.repository.CardsRepository;
 
 import java.util.List;
@@ -15,10 +21,20 @@ public class CardsController {
 
     @Autowired
     private CardsRepository cardsRepository;
+    @Autowired
+    private CardServiceConfig cardServiceConfig;
 
     @PostMapping("/myCards")
     public List<Cards> getCardsDetails(@RequestBody Customer customer){
 
         return cardsRepository.findByCustomerId(customer.getCustomerId());
+    }
+
+    @GetMapping("cards/properties")
+    public String getCardsProperties() throws JsonProcessingException {
+        ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(cardServiceConfig.getMsg(), cardServiceConfig.getBuildVersion(),
+                cardServiceConfig.getMailDetails(), cardServiceConfig.getActiveBranches());
+        return objectWriter.writeValueAsString(properties);
     }
 }
